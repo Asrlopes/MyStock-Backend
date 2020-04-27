d43695bef207fe1048383ee703520fda093aa135
Amoresio Souza <amoresio.souza@unemat.br>
2020-04-27 06:20:41 -0400

    model/migration de produtos

---
+++ src/database/migrations/1587982254050-CreateProducts.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateProducts1587982254050 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            generationStrategy: 'increment',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
---
+++ src/models/Products.ts
@@ -0,0 +1,11 @@
+class Products {
+  id: number;
+
+  name: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
+
+export default Products;
